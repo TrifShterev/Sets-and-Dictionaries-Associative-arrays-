3f9dfb6d58321708b0b56d3c5631d0b5e9237744
TrifShterev <tshterev@yahoo.com>
2020-09-25 11:28:57 +0300

    Add project files.

---
+++ Average Student Grades/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Average_Student_Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numberOfStudents = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<decimal>> studentsDictionary = new Dictionary<string, List<decimal>>();
+
+            for (int i = 0; i < numberOfStudents; i++)
+            {
+                var input = Console.ReadLine().Split();
+                var student = input[0];
+                var grade =decimal.Parse(input[1]);
+
+                if (!studentsDictionary.ContainsKey(student))
+                {
+                    studentsDictionary.Add(student, new List<decimal>());
+                }
+                studentsDictionary[student].Add(grade);
+
+            }
+            foreach (var (student,grades) in studentsDictionary)
+            {
+              Console.WriteLine($"{student} ->{string.Join(" ",grades)}(avg: {grades.Average()}) ");
+            }
+        }
+    }
+}
---
+++ Count Same Values in Array/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Count_Same_Values_in_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            Dictionary<double, int> dict = new Dictionary<double, int>();
+
+            foreach (var currentNumber in numbers)
+            {
+                if (!dict.ContainsKey(currentNumber))
+                {
+                    dict.Add(currentNumber, 0);
+                }
+                dict[currentNumber] += 1;
+            }
+            foreach (var (key,value) in dict)
+            {
+                Console.WriteLine($"{key} - {value} times");
+            }
+        }
+    }
+}
