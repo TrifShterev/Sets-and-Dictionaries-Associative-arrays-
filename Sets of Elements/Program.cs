90380ccfbba83e0f3951f88f0bc840f4c155bf0a
TrifShterev <tshterev@yahoo.com>
2020-09-28 16:08:35 +0300

    new tasks Added

---
+++ Sets of Elements/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Sets_of_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sets = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var firstSetCount = sets[0];
+            var secondSetCount = sets[1];
+
+            HashSet<int> setOne = new HashSet<int>();
+            HashSet<int> setSecond = new HashSet<int>();
+            for (int i = 0; i < firstSetCount; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                setOne.Add(num);
+            }
+            for (int i = 0; i < secondSetCount; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                setSecond.Add(num);
+            }
+            setOne.IntersectWith(setSecond);
+            
+            Console.WriteLine(string.Join(" ",setOne));
+
+            
+        }
+    }
+}
---
+++ Unique Usernames/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Unique_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> usernames = new HashSet<string>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string user = Console.ReadLine();
+                usernames.Add(user);
+            }
+            foreach (var name in usernames)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
